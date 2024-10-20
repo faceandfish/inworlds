5418a1b933d2597047a1fe1ae9942eaea42f9b8c
Miki <tangmeiqi925@gmail.com>
2024-10-20 19:05:25 +0900

    10.202

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -3,34 +3,20 @@ import { Metadata } from "next";
 import { getDictionary } from "../dictionaries";
 import { Locale, Namespace, Dictionary } from "../i18n-config";
 import HomePage from "@/components/Main/HomePage";
-import { GetServerSideProps } from "next";
-import { BookInfo } from "../lib/definitions";
 
 type Props = {
   params: { lang: Locale };
-  initialBooks: BookInfo[]; // 使用适当的类型替换 any[]
-  dict: Dictionary; // 使用适当的类型替换 any
 };
 
-export default function Home({ initialBooks, dict }: Props) {
-  return <HomePage initialBooks={initialBooks} />;
-}
-
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const lang = params?.lang as Locale;
+export default async function Home({ params }: Props) {
+  const lang = params.lang;
   const dict = await getDictionary(lang, "navbar" as Namespace);
 
   const initialBooksResponse = await fetchHomepageBooks(1);
   const initialBooks = initialBooksResponse.data.dataList;
 
-  return {
-    props: {
-      initialBooks,
-      dict,
-      params: { lang }
-    }
-  };
-};
+  return <HomePage initialBooks={initialBooks} />;
+}
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const lang = params.lang;
