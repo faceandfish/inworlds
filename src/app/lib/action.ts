cbc8b642a894f669f7c3ac92f15550233bc7e587
Miki <tangmeiqi925@gmail.com>
2024-11-08 22:32:44 +0900

    11.8

--- src/app/lib/action.ts
+++ src/app/lib/action.ts
@@ -41,10 +41,11 @@ let token: string | null = null;
 
 // 创建 axios 实例
 const api = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "development"
-      ? "http://192.168.0.103:8088/inworlds/api"
-      : "https://api.inworlds.xyz:8088/inworlds/api",
+  baseURL: "https://api.inworlds.xyz/inworlds/api",
+  // process.env.NODE_ENV === "development"
+  //   ? "http://192.168.0.103:8088/inworlds/api"
+  //   : "https://api.inworlds.xyz:8088/inworlds/api",
+
   headers: {
     "Content-Type": "application/json"
   },
