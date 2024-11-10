bb79959b1c6a319a590c92342c63976b11be504a
Miki <tangmeiqi925@gmail.com>
2024-11-10 13:55:47 +0900

    10.102

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,4 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "api.inworlds.xyz",
-//         port: "9000",
-//         pathname: "/book-bucket/**"
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "api.inworlds.xyz",
-//         port: "9000",
-//         pathname: "/avatar-bucket/**"
-//       },
-
-//       {
-//         protocol: "https",
-//         hostname: "randomuser.me",
-//         port: "",
-//         pathname: "/**"
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "lh3.googleusercontent.com",
-//         port: "",
-//         pathname: "/**"
-//       }
-//     ]
-//   }
-// };
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -45,7 +14,7 @@ const nextConfig = {
         port: "9000",
         pathname: "/avatar-bucket/**"
       },
-      // 这些外部服务的配置保持不变
+
       {
         protocol: "https",
         hostname: "randomuser.me",
@@ -59,14 +28,6 @@ const nextConfig = {
         pathname: "/**"
       }
     ]
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://192.168.0.103:8088/inworlds/api/:path*"
-      }
-    ];
   }
 };
 
